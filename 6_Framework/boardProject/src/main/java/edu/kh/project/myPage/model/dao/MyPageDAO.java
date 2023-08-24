c2de9cc581a59308edd5611f5efb31bd28765ff3
js3720 <dbstnrla1124@gmail.com>
2023-08-25 03:30:21 +0900

    KH230825

--- 6_Framework/boardProject/src/main/java/edu/kh/project/myPage/model/dao/MyPageDAO.java
+++ 6_Framework/boardProject/src/main/java/edu/kh/project/myPage/model/dao/MyPageDAO.java
@@ -60,5 +60,14 @@ public class MyPageDAO {
 		return sqlSession.update("myPageMapper.secession", memberNo);
 	}
 
+	/**
+	 * 프로필 이미지 수정
+	 * @param loginMember
+	 * @return result
+	 */
+	public int updateProfile(Member loginMember) {
+		return sqlSession.update("myPageMapper.updateProfileImage", loginMember);
+	}
+
 	
 }
