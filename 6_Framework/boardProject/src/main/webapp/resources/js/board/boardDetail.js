436a4c854564c4ffc9da4588f0d9922f377e4324
js3720 <dbstnrla1124@gmail.com>
2023-08-29 21:49:20 +0900

    kh230829

--- 6_Framework/boardProject/src/main/webapp/resources/js/board/boardDetail.js
+++ 6_Framework/boardProject/src/main/webapp/resources/js/board/boardDetail.js
@@ -52,3 +52,12 @@ boardList.addEventListener("click", e=>{
     }) // 예외 발생 시 처리하는 부분
 
 })
+
+// 게시글 수정 버튼 클릭 시
+const updateBtn = document.getElementById("updateBtn");
+
+updateBtn.addEventListener("click", ()=>{
+    location.href = location.pathname.replace("board","board2") +"/update"+location.search;
+
+    // /board2/1/1512/update?cp=1 (GET)
+})
---
+++ 6_Framework/boardProject/src/main/webapp/resources/js/board/boardList.js
@@ -0,0 +1,14 @@
+const insertBtn = document.getElementById("insertBtn");
+
+if(insertBtn != null){
+    // 글쓰기 버튼 클릭 시
+    insertBtn.addEventListener("click",()=>{
+        // JS BOM 객체 중 location
+
+        // location.href = "주소";
+        // 해당 주소 요청 (GET방식)
+
+        location.href = `/board2/${location.pathname.split("/")[2]}/insert`;
+                        //  /board2/1/insert
+    })
+}
