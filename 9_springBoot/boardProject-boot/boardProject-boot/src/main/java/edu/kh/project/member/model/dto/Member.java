2652f816673420c02326adf39bd7f231c49cc8ac
js3720 <dbstnrla1124@gmail.com>
2023-10-23 02:22:27 +0900

    spring boot 6

---
+++ 9_springBoot/boardProject-boot/boardProject-boot/src/main/java/edu/kh/project/member/model/dto/Member.java
@@ -0,0 +1,96 @@
+package edu.kh.project.member.model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Member {
+	private int memberNo;
+	private String memberEmail;
+	private String memberPw;
+	private String memberNickname;
+	private String memberTel;
+	private String memberAddress;
+	private String profileImage;
+	private String enrollDate;
+	private String memberDeleteFlag;
+	private String authority;
+	public Member() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	public String getMemberPw() {
+		return memberPw;
+	}
+	public void setMemberPw(String memberPw) {
+		this.memberPw = memberPw;
+	}
+	public String getMemberNickname() {
+		return memberNickname;
+	}
+	public void setMemberNickname(String memberNickname) {
+		this.memberNickname = memberNickname;
+	}
+	public String getMemberTel() {
+		return memberTel;
+	}
+	public void setMemberTel(String memberTel) {
+		this.memberTel = memberTel;
+	}
+	public String getMemberAddress() {
+		return memberAddress;
+	}
+	public void setMemberAddress(String memberAddress) {
+		this.memberAddress = memberAddress;
+	}
+	public String getProfileImage() {
+		return profileImage;
+	}
+	public void setProfileImage(String profileImage) {
+		this.profileImage = profileImage;
+	}
+	public String getEnrollDate() {
+		return enrollDate;
+	}
+	public void setEnrollDate(String enrollDate) {
+		this.enrollDate = enrollDate;
+	}
+	public String getMemberDeleteFlag() {
+		return memberDeleteFlag;
+	}
+	public void setMemberDeleteFlag(String memberDeleteFlag) {
+		this.memberDeleteFlag = memberDeleteFlag;
+	}
+	public String getAuthority() {
+		return authority;
+	}
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+	@Override
+	public String toString() {
+		return "Member [memberNo=" + memberNo + ", memberEmail=" + memberEmail + ", memberPw=" + memberPw
+				+ ", memberNickname=" + memberNickname + ", memberTel=" + memberTel + ", memberAddress=" + memberAddress
+				+ ", profileImage=" + profileImage + ", enrollDate=" + enrollDate + ", memberDeleteFlag="
+				+ memberDeleteFlag + ", authority=" + authority + "]";
+	}
+
+	
+}
